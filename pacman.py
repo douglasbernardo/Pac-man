5429af5086204f2c575a8076029a6b9ad8ec621c
douglasbernardo <43753293+douglasbernardo@users.noreply.github.com>
2020-06-13 00:02:26 -0300

    movimentação do personagem

--- pacman.py
+++ pacman.py
@@ -1,14 +1,35 @@
 import pygame
 
+#constantes
 AMARELO = (255,255,0)
+PRETO = (0,0,0)
+VELOCIDADE = 1
+RAIO = 30
+
 pygame.init()
 
-tela = pygame.display.set_mode((640,480), 0)
-
+tela = pygame.display.set_mode((640,480), 0) #tela 
+x = 10
+y = 10
+vel_x = VELOCIDADE
+vel_y = VELOCIDADE
 while True:
     #Calcular as regras
+    x += vel_x
+    y += vel_y
+    if x + RAIO > 640:
+        vel_x -= VELOCIDADE
+    
+    if x - RAIO < 0:
+        vel_x = VELOCIDADE
+    
+    if y + RAIO > 480:
+        vel_y -= VELOCIDADE
+    if y - RAIO < 0:
+        vel_y = VELOCIDADE
     #pinta
-    pygame.draw.circle(tela,AMARELO,(320,240), 50, 0)
+    tela.fill(PRETO)
+    pygame.draw.circle(tela,AMARELO,(int(x),int(y)), RAIO, 0)
     pygame.display.update()
     #eventos
 
